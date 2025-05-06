ca1f17b08f931aa98e870968497fd907db3a1d83
Joao Baptista <joao.alves.baptista@gmail.com>
2025-05-06 14:19:08 +0100

    first version model

---
+++ BackEnd/src/main/java/com/store/store/model/Item.java
@@ -0,0 +1,10 @@
+package com.store.store.model;
+
+public class Item {
+
+    private int id;
+    private String title;
+    private String location;
+    private String category;
+    private Long quantity;
+}
